8c364d8a4a4ee9b499dcabc52d47f5b5c83ec571
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2022-05-30 15:56:42 +0300

    Updates

---
+++ precache-manifest.2fbff8857e05840e2cbf5541fdd3544a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3a17615c516d1064fe56c311ec622e",
+    "url": "/checklist/index.html"
+  },
+  {
+    "revision": "80cd1725c8c6c1a9c5af",
+    "url": "/checklist/static/css/main.95142db4.chunk.css"
+  },
+  {
+    "revision": "6aaabe91e384945ea8e6",
+    "url": "/checklist/static/js/2.1b2af5c3.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/checklist/static/js/2.1b2af5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80cd1725c8c6c1a9c5af",
+    "url": "/checklist/static/js/main.0384c53a.chunk.js"
+  },
+  {
+    "revision": "965c9ef72500a4f41e98",
+    "url": "/checklist/static/js/runtime-main.4db2edd8.js"
+  }
+]);
