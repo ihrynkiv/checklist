25f3bcb577775d9f7f67e43ffdeccaec51f2b3d5
Ivan Hrynkiv <ivan.hrynkiv@gmail.com>
2022-05-31 11:03:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const TABS_NAMES = Object.keys(TABS_MAP);
 
 const keyMap = {
   CLEAR_REVIEW: ["del", "backspace"],
-  DELETE_REVIEW: ["shift+del", "shift+backspace"],
+  DELETE_REVIEW: ["c", "b"],
   NAVIGATION: ["alt+1", "alt+2", "alt+3", "alt+4", "alt+5", "alt+6", "alt+7", "alt+8", "alt+9", "alt+0"],
   RIGHT: ["right", "d", "l"],
   LEFT: ["left", "a", "h"],
@@ -154,8 +154,10 @@ function App() {
   const handlers = {
     CLEAR_REVIEW: () => {
       clearHandler(localStorage.getItem('activeReview') - 1)
+      console.log('clear')
     },
     DELETE_REVIEW: () => {
+      console.log('delete')
       const activeId = +localStorage.getItem('activeReview') || 0
       deleteHandler(activeId)
     },
